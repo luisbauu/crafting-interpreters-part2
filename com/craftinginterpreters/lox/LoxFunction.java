4b050054ba5a1ba7e659d924b1fc120ed1f2d065
Dru Villamor <jdsvillamor@gmail.com>
2022-10-11 22:17:09 +0800

    bump

--- com/craftinginterpreters/lox/LoxFunction.java
+++ com/craftinginterpreters/lox/LoxFunction.java
@@ -12,30 +12,32 @@ class LoxFunction implements LoxCallable {
     this.declaration = declaration;
   }
 
-@Override
-public Object call(Interpreter interpreter,
-                    List<Object> arguments) {
-  Environment environment = new Environment(closure);
-  for (int i = 0; i < declaration.params.size(); i++) {
-    environment.define(declaration.params.get(i).lexeme,
-        arguments.get(i));
+
+  @Override
+  public Object call(Interpreter interpreter,
+                      List<Object> arguments) {
+    Environment environment = new Environment(closure);
+    for (int i = 0; i < declaration.params.size(); i++) {
+      environment.define(declaration.params.get(i).lexeme,
+          arguments.get(i));
+    }
+
+    try {
+      interpreter.executeBlock(declaration.body, environment);
+    } catch (Return returnValue) {
+      return returnValue.value;
+    }
+
+    return null;
   }
 
-  try {
-    interpreter.executeBlock(declaration.body, environment);
-  } catch (Return returnValue) {
-    return returnValue.value;
+  @Override
+  public int arity() {
+    return declaration.params.size();
   }
 
-  return null;
-}
-
-@Override
-public int arity() {
-  return declaration.params.size();
-}
-
-@Override
-public String toString() {
-  return "<fn " + declaration.name.lexeme + ">";
+  @Override
+  public String toString() {
+    return "<fn " + declaration.name.lexeme + ">";
+  }
 }
